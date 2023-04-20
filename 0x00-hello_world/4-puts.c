459735d962a68aa896280823324d27fb288be4af
Baseerah59 <Baseerah59>
2023-04-20 09:01:40 -0700

    good

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-*main - main entry point
+*main - entry point
 *return: always 0 (success)
+*Description: the program will print exactly what is in the quote
 */
 int main(void)
 {
-	puts('"programming is like building a multilingual puzzle');
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
